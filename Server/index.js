8bdafdc86bcdf8995cbfea4afb99eeb3ac0dc342
TawiahObedCodeX <tawiaho858@gmail.com>
2025-11-24 02:02:47 +0000

    almost done

--- Server/index.js
+++ Server/index.js
@@ -1,22 +1,27 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import AdminAuth from './Routes/AdminAuth.js';
+import ClientAuth from './Routes/ClientAuth.js';
 
-
+dotenv.config();
 
 const Server = express();
 
-// middleware
+// Middleware
 Server.use(express.json());
 Server.use(cookieParser());
+Server.use('/auth/admin', AdminAuth);
+Server.use('/auth/client', ClientAuth);
 
-// testing,v-0  mv 
+// Test Route
 Server.get("/", (req, res) => {
   res.send(`Hello world try out Melo's Spices , it's still on work`);
 });
 
+// Corrected fallback port
 const Port = process.env.PORT || 5000;
 
-Server.listen(Port, "localhost", () => {
+Server.listen(Port, "127.0.0.1", () => {
   console.log(`server is running at http://localhost:${Port}`);
 });
