92709e8abc9760d1807aa02137ee9e10314725a6
TawiahObedCodeX <tawiaho858@gmail.com>
2025-11-30 23:53:58 +0000

    I was able to figure out how to create multiple tables on one database

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -12,13 +12,13 @@ const pool = new Pool({
 });
 
 // Database connected
-pool.on("connect", () => {
-  console.log("Connected to PostgreSQL database");
-});
-
-// Database error
-pool.on("error", (err) => {
-  console.error("PostgreSQL error:", err);
-});
+(async () => {
+  try {
+    const check = await pool.query("SELECT current_database(), version()");
+    console.log("Connected to PostgreSQL database:", check.rows[0].current_database);
+  } catch (err) {
+    console.error("Database connection error:", err);
+  }
+})();
 
 export default pool;
